f21755f8ebfb75dcd7aa81ea9e6422c9202a0cbf
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-04 18:43:56 +0300

    Создание класса содержащего отфильтрованные объекты

---
+++ DesignPatterns/BetterFilter.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.Specification;
+
+namespace DesignPatterns;
+
+public class BetterFilter
+{
+	public IEnumerable<Product> Filter(
+		IEnumerable<Product> items, 
+		ISpecification<Product> spec)
+	{
+		foreach (var i in items)
+			if (spec.IsSatisfiedBy(i))
+				yield return i;
+	}
+}
