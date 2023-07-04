16d97df826f2b8bec252ef4cf6d28c95934e11cf
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-04 18:43:22 +0300

    Набор реализованных спецификация

---
+++ DesignPatterns/Specification/SpecificationEntity/ColorSpecification.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Specification.SpecificationEntity;
+
+public class ColorSpecification : CompositeSpecification<Product>
+{
+	private Color color;
+
+	public ColorSpecification(Color color)
+	{
+		this.color = color;
+	}
+
+	public override bool IsSatisfiedBy(Product item)
+	{
+		return item.Color == color;
+	}
+}
---
+++ DesignPatterns/Specification/SpecificationEntity/OrNotSpecification.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Specification.SpecificationEntity;
+
+public class OrNotSpecification<T> : CompositeSpecification<T>
+{
+	readonly ISpecification<T> _left;
+	readonly ISpecification<T> _right;
+
+	public OrNotSpecification(ISpecification<T> left, ISpecification<T> right)
+	{
+		_left = left;
+		_right = right;
+	}
+
+	public override bool IsSatisfiedBy(T candidate) => _left.IsSatisfiedBy(candidate) || !_right.IsSatisfiedBy(candidate);
+}
