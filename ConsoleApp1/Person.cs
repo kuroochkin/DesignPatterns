fe158dd8100ad261e6ed2800c2e9f72af974dd13
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-15 20:25:57 +0300

    Programm

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Builder;
+
+public class Person
+{
+	public Person()
+	{
+		Console.WriteLine("Creating an instance of Person");
+	}
+
+	// address
+	public string StreetAddress, Postcode, City;
+
+	// employment info
+	public string CompanyName, Position;
+
+	public int AnnualIncome;
+
+	public override string ToString()
+	{
+		return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}," +
+			   $" {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}," +
+			   $" {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+	}
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Builder;
+using Builder.CodeBuilder;
+using Builder.HtmlBuilder;
+using Builder.PersonBuilder;
 
 var words = new[] { "hello", "world" };
 var builder = new HtmlBuilder("ul");
@@ -7,3 +10,22 @@ foreach (var word in words)
 	builder.AddChild("li", word);
 
 Console.WriteLine(builder.ToString());
+
+
+//Строим Person
+var pb = new PersonBuilder();
+
+Person person = pb
+	.Lives
+		.At("123 London Road")
+		.In("London")
+		.WithPostcode("SW12")
+	.Works
+		.At("Fabrikam")
+		.AsA("Engineer")
+		.Earning(123000);
+
+Console.WriteLine(person);
+
+var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+Console.WriteLine(cb);
