ebbe7427ec85d547bc79568226c5e5b80dbdbc6b
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-15 23:35:45 +0300

    Реализация абстрактной фабрики

---
+++ Factory/Program.cs
@@ -0,0 +1,8 @@
+﻿using AbstractFactory.Factory;
+
+var basic = ShapeFactory.GetFactory(false);
+var basicTriangle = basic.Create(Shape.Triangle);
+basicTriangle.Draw();
+
+var roundedSquare = ShapeFactory.GetFactory(true).Create(Shape.Square);
+roundedSquare.Draw();
