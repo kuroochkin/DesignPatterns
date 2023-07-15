6da4183c8dc4fd4e1415758970f877281a64d6d3
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-15 23:35:25 +0300

    Фабрики внутри абстрактной

---
+++ Factory/Factory/ShapeFactory.cs
@@ -0,0 +1,21 @@
+﻿using Factory;
+
+namespace AbstractFactory.Factory;
+
+public enum Shape
+{
+    Square,
+    Triangle
+}
+
+public abstract class ShapeFactory
+{
+    public abstract IShape Create(Shape shape);
+    public static ShapeFactory GetFactory(bool rounded)
+    {
+        if (rounded)
+            return new RoundedShapeFactory();
+        else
+            return new BasicShapeFactory();
+    }
+}
