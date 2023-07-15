78d4b300fc7913b225a5994835e045d1e4527286
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-15 20:25:33 +0300

    PersonBuilder

---
+++ ConsoleApp1/PersonBuilder/PersonJobBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace Builder.PersonBuilder;
+
+public class PersonJobBuilder : PersonBuilder
+{
+	public PersonJobBuilder(Person person) : base(person)
+	{
+	}
+
+	public PersonJobBuilder At(string companyName)
+	{
+		person.CompanyName = companyName;
+		return this;
+	}
+
+	public PersonJobBuilder AsA(string position)
+	{
+		person.Position = position;
+		return this;
+	}
+
+	public PersonJobBuilder Earning(int annualIncome)
+	{
+		person.AnnualIncome = annualIncome;
+		return this;
+	}
+}
