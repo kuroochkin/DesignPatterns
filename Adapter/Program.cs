e1f9854b7095d8f6b2b9031286baf9e0448f1f50
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-19 17:19:50 +0300

    Кэширование адаптера

---
+++ Adapter/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Adapter;
+using Adapter.Adapter;
+
+List<VectorObject> vectorObjects
+	  = new List<VectorObject>
+	{
+	  new VectorRectangle(1, 1, 10, 10),
+	  new VectorRectangle(3, 3, 6, 6)
+	};
+
+List<Point> points = new List<Point>();
+
+
+void DrawPoint(Point p)
+{
+	//simulation
+	Console.Write(".");
+}
+
+void Draw()
+{
+	foreach (var vo in vectorObjects)
+	{
+		foreach (var line in vo)
+		{
+			var adapter = new LineToPointAdapter(line);
+			foreach(var ad in adapter)
+			{
+				DrawPoint(ad);
+			}
+		}
+	}
+}
+
+Draw(); 
