f3f7882fa558c00c623f3f6f15cb6d9326b4ec99
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-03 23:48:45 +0300

    Создание декоратора

--- _Decorator/Post.cs
+++ _Decorator/Post.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _Decorator;
 
-namespace _Decorator
+public class Post
 {
-	internal class Post
-	{
-	}
+	public int UserId { get; set; }
+	public int Id { get; set; }
+	public string? Title { get; set; }
+	public string? Body { get; set; }
+	public override string ToString() => $"{Id} - {Title}";
 }
---
+++ _Decorator/PostServiceDecorator.cs
@@ -0,0 +1,14 @@
+﻿namespace _Decorator;
+
+//Непосредственно декоратор сервиса постов
+public abstract class PostServiceDecorator : IPostService
+{
+	protected readonly IPostService postService;
+
+	public PostServiceDecorator(IPostService postService)
+	{
+		this.postService = postService;
+	}
+
+	public abstract Task<Post?> GetPost(int postId);
+}
