b6ac60e23cf5805fc133807c7afc7f0815d7f7af
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-07 19:19:10 +0300

    Реализация абстрактного класса Education

---
+++ TemplateMethod/Education.cs
@@ -0,0 +1,17 @@
+﻿namespace TemplateMethod;
+
+public abstract class Education
+{
+	public virtual void Enter() { }
+	public virtual void Study() { }
+	public virtual void PassExams() { }
+	public virtual void GetDocument() { }
+
+	public void Learn()
+	{
+		Enter();
+		Study();
+		PassExams();
+		GetDocument();
+	}
+}
