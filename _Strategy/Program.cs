ce1dac7eb3bf24c23c9882fa28d4ccd7c48e5bde
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-07 19:00:59 +0300

    Реализация стратегии

---
+++ _Strategy/Program.cs
@@ -0,0 +1,13 @@
+﻿using _Strategy;
+
+var context = new Context();
+
+Console.WriteLine("Client: Strategy is set to normal sorting.");
+context.SetStrategy(new StrategyA());
+context.DoSomeBusinessLogic();
+
+Console.WriteLine();
+
+Console.WriteLine("Client: Strategy is set to reverse sorting.");
+context.SetStrategy(new StrategyB());
+context.DoSomeBusinessLogic();
---
+++ _Strategy/StrategyA.cs
@@ -0,0 +1,12 @@
+﻿namespace _Strategy;
+
+public class StrategyA : IStrategy
+{
+	public object DoAlgorithm(object data)
+	{
+		var list = data as List<string>;
+		list.Sort();
+
+		return list;
+	}
+}
